675ade8821146a1564eaa5ac3da4aa853d2b4b74
SohmenL <lucia.sohmen@hvf-bs.net>
2017-09-20 11:49:09 +0200

    added date, fixed hindawi links

---
+++ src/main/java/metadata/PublicationDate.java
@@ -0,0 +1,17 @@
+package metadata;
+
+import lombok.Getter;
+import lombok.Setter;
+/**
+ * Created by SohmenL on 15.09.2017.
+ */
+@Getter
+@Setter
+public class PublicationDate {
+    String day;
+    String month;
+    String year;
+    public String toString(){
+        return("day: "+day+"month: "+month+"year: "+year);
+    }
+}
